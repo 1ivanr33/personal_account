b2313411b8a2faae7bc901cb3a6ef69f19970b28
Makhnachev Andrey <MakhnachevAN@it.mos.ru>
2018-09-26 19:26:24 +0300

    Для взаимодействия с сервисом аутентификации используется XMLHttpRequest, а не fetch.

--- src/loginForm/loginForm.js
+++ src/loginForm/loginForm.js
@@ -14,7 +14,7 @@ class LoginForm extends React.Component {
 		this.onSubmit = this.onSubmit.bind(this);
 	}
 
-	onSubmit(e){
+	onSubmitOld(e){
 		const url_post = "http://10.82.186.67:7001/war/resources/AdministrationService/getOperatorByLoginParams";
 
 		let requestData = {
@@ -40,7 +40,50 @@ class LoginForm extends React.Component {
 		e.preventDefault();
 	}
 
+    onSubmit(e) {
+        var requestData = {
+            Login: this.state.Login,
+            Password: this.state.Password
+        }
 
+        function makeRequest (method, url) {
+            return new Promise(function (resolve, reject) {
+                var xhr = new XMLHttpRequest();
+
+                xhr.open(method, url);
+                xhr.setRequestHeader("Content-Type", "application/json");
+////                xhr.send('{"Login":"Andrew","Password":"welcome1"}');
+                xhr.send(JSON.stringify(requestData));
+
+                xhr.onload = function () {
+                    if (this.status === 200) {
+////                         resolve(xhr.response);
+                        resolve(xhr);
+                    } else {
+                        reject({
+                            status: this.status,
+                            statusText: xhr.statusText
+                        });
+                    }
+                };
+                xhr.onerror = function () {
+                    reject({
+                        status: this.status,
+                        statusText: xhr.statusText
+                    });
+                };
+//                 xhr.send();
+            });
+        }
+
+        makeRequest('POST', 'http://localhost:7001/war/resources/AdministrationService/getOperatorByLoginParams')
+        //             .then(response => {console.log('Response - ' + response); console.log('Security_Token - ' + response.get('Security_Token'));})
+            .then(response => { console.log('Response - ' + response.responseText); console.log('Security_Token - ' + response.getResponseHeader('Security_Token')); })
+            .catch(function (err) {
+                console.error('Augh, there was an error!', err.statusText);
+            });
+
+    }
 	onLoginChange(e) {
 		this.setState({Login: e.target.value});
 	}
@@ -58,7 +101,7 @@ class LoginForm extends React.Component {
 
 
 
-			<form onSubmit={this.onSubmit}>
+			<div onSubmit={this.onSubmit}>
 				<h3>Вход</h3>
 				<p className={this.state.Login ? 'dirty' : ''}><input id="login" type="text" name="login" value={this.state.Login}
 										 onChange={this.onLoginChange}/><label for="login" className='textLabel'> E-mail или СНИЛС </label></p>
@@ -66,7 +109,7 @@ class LoginForm extends React.Component {
 										  onChange={this.onPasswordChange}/><label for="password" className='textLabel'> Пароль </label></p>
 				<p><input id="foreign" type="checkbox"/><label for="foreign">Чужой компьютер </label><span className='passwordRecover'>Восстановить пароль</span> </p>
 				<p><input type="submit" value="Войти" onClick={this.onSubmit}/></p>
-			</form>
+			</div>
 
 
 		);
