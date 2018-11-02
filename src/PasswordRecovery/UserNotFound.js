966254bcc27c0dffc31220adde31544be637e6dd
Rakcheev Ivan <rakcheevib@altarix.ru>
2018-11-02 16:48:52 +0300

    Работы по задаче ELK-523.

---
+++ src/PasswordRecovery/UserNotFound.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import './UserNotFound.scss';
+import Notice from '../Notice';
+import FooterNavBar from '../FooterNavBar/FooterNavBar';
+import { Link } from 'react-router-dom';
+import { inject, observer } from "mobx-react";
+import MaskedInput from 'react-text-mask';
+import emailMask from 'text-mask-addons/dist/emailMask';
+
+@inject("Store")
+@observer
+class UserNotFound extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			Email: ''
+		};
+		this.onSubmit = this.onSubmit.bind(this);
+		this.onEmailChange = this.onEmailChange.bind(this);
+	}
+
+	onSubmit(e){
+		const url_post = "http://igitb1700000221.hq.corp.mos.ru:7001/war/resources/AdministrationService/getOperatorByLoginParams";
+
+		let requestData = {
+			email: this.state.Email
+		}
+
+		let postData = {
+			method: 'POST',
+			body: JSON.stringify(requestData),
+			headers:{
+				'Content-Type': 'application/json'
+			}
+		}
+
+		fetch(url_post, postData).then(res => res.json())
+			.then(response => {
+				console.log('Success:', JSON.stringify(response))
+				this.props.history.push("/home");
+			})
+
+			.catch((error) => {
+				console.error(error);
+			});
+
+		e.preventDefault();
+	}
+
+	onEmailChange(e) {
+		this.setState({Email: e.target.value});
+	}
+
+	handleKeyPress(target) {
+		if(target.charCode===13){
+			this.onSubmit();
+		}
+	}
+
+
+	render() {
+
+		return (
+
+			<div className='userNotFound'>
+				<div className='userNotFoundWrapper'>
+
+					<div className='mainFormUserNotFound' onKeyPress={this.handleKeyPress} onSubmit={this.onSubmit}>
+
+						<h3>Восстановление пароля</h3>
+						<p>
+							Введите e-mail, который вы указывали при регистрации. Мы отправим вам письмо с инструкцией по восстановлению пароля
+						</p>
+						<p className={this.state.Email ? 'dirty' : ''}>
+							<MaskedInput mask={emailMask} id="login" type="text" name="login" value={this.state.Login}
+										 onChange={this.onEmailChange}/>
+							<label htmlFor="login" className='textLabel'> E-mail </label>
+						</p>
+
+
+						<p className='recoverPasswordBtn'><input type="submit" value="Восстановить пароль" onClick={this.onSubmit}/></p>
+						<p className='alternate'><Link to="/">Я вспомнил пароль</Link></p>
+						<Notice>
+							<h3>Пользователь не найден</h3>
+						</Notice>
+					</div>
+
+				</div>
+				<FooterNavBar/>
+			</div>
+
+		);
+	}
+}
+
+export default UserNotFound;
--- src/Router.js
+++ src/Router.js
@@ -6,6 +6,7 @@ import Home from './Home';
 import UserProfile from './ProfileSelect/UserProfile';
 import CompanyProfile from './ProfileSelect/CompanyProfile';
 import PasswordRecovery from './PasswordRecovery/PasswordRecovery';
+import UserNotFound from './PasswordRecovery/UserNotFound';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
 import { Provider } from 'mobx-react';
 import RootStore from './stores/RootStore';
@@ -25,39 +26,6 @@ import RootStore from './stores/RootStore';
 	</div>
 )*/
 
-const Topic = ({ match }) => (
-	<div>
-		<h3>{match.params.topicId}</h3>
-	</div>
-)
-
-export const Topics = ({ match }) => (
-	<div>
-		<h2>Topics</h2>
-		<ul>
-			<li>
-				<Link to={`${match.url}/rendering`}>
-					Rendering with React
-				</Link>
-			</li>
-			<li>
-				<Link to={`${match.url}/components`}>
-					Components
-				</Link>
-			</li>
-			<li>
-				<Link to={`${match.url}/props-v-state`}>
-					Props v. State
-				</Link>
-			</li>
-		</ul>
-
-		<Route path={`${match.path}/:topicId`} component={Topic}/>
-		<Route exact path={match.path} render={() => (
-			<h3>Please select a topic.</h3>
-		)}/>
-	</div>
-)
 
 const Router = () => (
 
@@ -79,6 +47,7 @@ const Router = () => (
 					<Route path="/UserProfile" component={UserProfile}/>
 					<Route path="/CompanyProfile" component={CompanyProfile}/>
 					<Route path="/PasswordRecovery" component={PasswordRecovery}/>
+					<Route path="/UserNotFound" component={UserNotFound}/>
 				</Desktop>
 
 			</div>
