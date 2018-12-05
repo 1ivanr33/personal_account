4bb4b937d82ae7e456c01e4ea3c3389d3a1ca27c
Khusamov Sukhrob <khusamovsa@it.mos.ru>
2018-12-05 13:19:14 +0300

    config-overrides.js: Добавлено удаление бабель-плагина transform-decorators-legacy для работы TypeScript-версии проекта

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,6 @@
 const rewireSass = require('react-app-rewire-scss');
 const rewireMobX = require('react-app-rewire-mobx');
-const {injectBabelPlugin} = require('react-app-rewired');
+const {injectBabelPlugin, getBabelLoader} = require('react-app-rewired');
 
 /* config-overrides.js */
 module.exports = function override(config, env) {
@@ -10,6 +10,15 @@ module.exports = function override(config, env) {
 
 	config = injectBabelPlugin("babel-plugin-styled-components", config);
 	config = rewireMobX(config,env);
+
+	// Удаление бабель-плагина transform-decorators-legacy.
+    const loader = getBabelLoader(config.module.rules);
+    const options = loader.options || loader.query;
+    if (options.plugins) {
+        options.plugins = (
+            options.plugins.filter(plugin => plugin !== 'transform-decorators-legacy')
+		);
+	}
 
 	return config;
 }
