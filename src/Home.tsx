311002190bc1d72910e0c463096a0640a106487b
Rakcheev Ivan <rakcheevib@altarix.ru>
2018-12-20 14:56:39 +0300

    Работы по задаче ELK-613.

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,10 +3,53 @@ import './Home.scss';
 import { inject, observer } from "mobx-react";
 import {RouteComponentProps, withRouter} from "react-router-dom";
 import {IMobxProviderInjectedProps} from './MobxProvider';
+import {TRouteComponentProps} from './TRouteComponentProps';
 
 @inject("rootStore")
 @observer
-class Home extends React.Component<RouteComponentProps & IMobxProviderInjectedProps> {
+class Home extends React.Component<TRouteComponentProps & IMobxProviderInjectedProps> {
+
+	constructor(props: TRouteComponentProps  & IMobxProviderInjectedProps) {
+		super(props);
+		this.isSessionValid = this.isSessionValid.bind(this);
+	}
+
+	async isSessionValid() {
+
+		const url_post = "http://igitb1700000221.hq.corp.mos.ru:7001/war/resources/AdministrationService/getSessionNotExpired";
+		const {rootStore} = this.props;
+		if (!rootStore) throw new Error('rootStore не определен');
+		let tokenValue = window[rootStore.BrowserStorageType].getItem('securityToken');
+
+		let requestData = {
+			SecurityToken: tokenValue
+		};
+
+		let postData = {
+			method: 'POST',
+			body: JSON.stringify(requestData),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		};
+
+		const response = await fetch(url_post, postData);
+		const data = await response.json();
+		let errorCode = data.NotExpired;
+		console.log('errorCode - ' + errorCode);
+		if (errorCode == true) {
+			console.log('SessionNotExpired');
+			setTimeout(this.isSessionValid, 30000);
+		} else {
+			sessionStorage.clear();
+			localStorage.clear();
+			this.props.history.push("/");
+		}
+	}
+
+	 componentDidMount() {
+		 return this.isSessionValid();
+	}
 
 	componentWillMount() {
 		if (!( localStorage.getItem('securityToken') || (sessionStorage.getItem('securityToken')))){
--- src/ProfileSelect/UserProfile.tsx
+++ src/ProfileSelect/UserProfile.tsx
@@ -4,8 +4,56 @@ import {Link} from 'react-router-dom';
 import {TRouteComponentProps} from '../TRouteComponentProps';
 import UserProfileForm from '../Profiles/UserProfileForm';
 import BreadCrumbs from '../BreadCrumbs/BreadCrumbs';
+import {IMobxProviderInjectedProps} from '../MobxProvider';
+import { inject, observer } from "mobx-react";
 
-class UserProfile extends React.Component<TRouteComponentProps> {
+@inject("rootStore")
+@observer
+class UserProfile extends React.Component<TRouteComponentProps & IMobxProviderInjectedProps> {
+
+	constructor(props: TRouteComponentProps) {
+		super(props);
+		this.isSessionValid = this.isSessionValid.bind(this);
+	}
+
+
+	async isSessionValid() {
+
+		const url_post = "http://igitb1700000221.hq.corp.mos.ru:7001/war/resources/AdministrationService/getSessionNotExpired";
+		const {rootStore} = this.props;
+		if (!rootStore) throw new Error('rootStore не определен');
+		let tokenValue = window[rootStore.BrowserStorageType].getItem('securityToken');
+
+		let requestData = {
+			SecurityToken: tokenValue
+		};
+
+		let postData = {
+			method: 'POST',
+			body: JSON.stringify(requestData),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		};
+
+		const response = await fetch(url_post, postData);
+		const data = await response.json();
+		let errorCode = data.NotExpired;
+		console.log('errorCode - ' + errorCode);
+		if (errorCode == true) {
+			console.log('SessionNotExpired');
+			setTimeout(this.isSessionValid, 30000);
+		} else {
+			sessionStorage.clear();
+			localStorage.clear();
+			this.props.history.push("/");
+		}
+	}
+
+	 componentDidMount() {
+		 return this.isSessionValid();
+	}
+
 	render() {
 		if (!( localStorage.getItem('securityToken') || (sessionStorage.getItem('securityToken')))){
 			this.props.history.push("/");
