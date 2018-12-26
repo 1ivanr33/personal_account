7337518ea512c9897dad661c3abf9ec1347cdd43
Rakcheev Ivan <rakcheevib@altarix.ru>
2018-12-26 09:09:52 +0300

    Начало добавления метода getModules.

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,10 +3,55 @@ import './Home.scss';
 import { inject, observer } from "mobx-react";
 import {RouteComponentProps, withRouter} from "react-router-dom";
 import {IMobxProviderInjectedProps} from './MobxProvider';
+import {TRouteComponentProps} from './TRouteComponentProps';
 
 @inject("rootStore")
 @observer
-class Home extends React.Component<RouteComponentProps & IMobxProviderInjectedProps> {
+class Home extends React.Component<TRouteComponentProps & IMobxProviderInjectedProps> {
+	constructor(props: TRouteComponentProps & IMobxProviderInjectedProps) {
+		super(props);
+		this.getModules = this.getModules.bind(this);
+	}
+
+	async getModules() {
+
+		const url = "http://igitb1700000221.hq.corp.mos.ru:7001/war/resources/AdministrationService/getModules";
+		const {rootStore} = this.props;
+		if (!rootStore) throw new Error('rootStore не определен');
+		let tokenValue = window[rootStore.BrowserStorageType].getItem('securityToken');
+
+		let requestData = {
+			Security_Token: tokenValue
+		};
+
+		let postData = {
+			method: 'POST',
+			body: JSON.stringify(requestData),
+			headers: {
+				'Content-Type': 'application/json',
+				'Security_Token': tokenValue
+			}
+		};
+
+		const response = await fetch(url, postData);
+		const data = await response.json();
+		let errorCode = data.operationResult.ErrorCode;
+		let moduleOne = data.moduleProperty[0];
+		let moduleTwo = data.moduleProperty[1];
+		let moduleThree = data.moduleProperty[2];
+		console.log(errorCode);
+		/*if (errorCode == 0) {
+			//if(){}
+			console.log('SessionNotExpired');
+		} else {
+
+			//this.props.history.push("/");
+		}*/
+	}
+
+	componentDidMount() {
+		return this.getModules();
+	}
 
 	componentWillMount() {
 		if (!( localStorage.getItem('securityToken') || (sessionStorage.getItem('securityToken')))){
